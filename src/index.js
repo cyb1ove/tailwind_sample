0e809db59c8aa5fe4d0cd4c3f49a307b462cdc46
cyb1ove <cyb1ove.ua@gmail.com>
2021-12-06 19:18:34 +0200

    Alpine is working

--- src/index.js
+++ src/index.js
@@ -2,13 +2,33 @@ import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
-Alpine.start();
-
 function form() {
   return {
-    a: 'fsdf',
-    b: 3,
+    users: {},
+    email: {
+      value: '',
+      error: false,
+    },
+    password: {
+      value: '',
+      error: false,
+    },
+    init() {
+      this.users = JSON.parse(localStorage.getItem('users'));
+    },
+    verifyEmail() {
+      return !Object.prototype.hasOwnProperty.call(this.users, this.email.value);
+    },
+    submit() {
+      if (this.verifyEmail()) {
+        this.email.error = true;
+      } else if (this.password.value !== this.users[this.email.value]) {
+        this.value.error = true;
+      }
+    },
   };
 }
 
 Alpine.data('form', form);
+
+Alpine.start();
