83fde160d7598f7b1effac1cd9a56826aef84f2d
cyb1ove <cyb1ove.ua@gmail.com>
2021-12-09 11:11:05 +0200

    tailwindcss.config added

---
+++ tailwindcss.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontSize: {
+      xs: '.58rem',
+    },
+    textColor: {
+      white: '#FFF',
+      black: '#000',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
