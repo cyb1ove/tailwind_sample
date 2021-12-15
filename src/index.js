9de982d0fc58b12046e4416272b6a38d397e8bf9
cyb1ove <cyb1ove.ua@gmail.com>
2021-12-15 19:08:34 +0200

    Improve

--- src/index.js
+++ src/index.js
@@ -1,105 +1,94 @@
-/* eslint-disable guard-for-in */
+// /* eslint-disable guard-for-in */
 import Alpine from 'alpinejs';
 
-Alpine.store('data', {
-  inputs: {
-    email: {
-      value: '',
-      error: false,
-      completed: false,
-    },
-    password: {
-      value: '',
-      error: false,
-      completed: false,
-    },
-    registration: false,
-    message: 'Welcome',
-  },
-});
+window.Alpine = Alpine;
+
+// Alpine.store('data', {
+//   inputs: {
+//     email: {
+//       value: '',
+//       error: false,
+//       completed: false,
+//     },
+//     password: {
+//       value: '',
+//       error: false,
+//       completed: false,
+//     },
+//     registration: false,
+//     message: 'Welcome',
+//   },
+// });
 
 Alpine.store('behavior_methods', {
-  data: Alpine.store('data'),
-  verifyUser() {
-    const email = this.data.inputs.email.value;
+  // data: Alpine.store('data'),
+  // verifyUser() {
+  //   const email = this.data.inputs.email.value;
 
-    return Boolean(localStorage.getItem(email));
-  },
-  verifyPassword() {
-    const email = this.data.inputs.email.value;
-    const password = this.data.inputs.password.value;
+  //   return Boolean(localStorage.getItem(email));
+  // },
+  // verifyPassword() {
+  //   const email = this.data.inputs.email.value;
+  //   const password = this.data.inputs.password.value;
 
-    return password === localStorage.getItem(email);
-  },
+  //   return password === localStorage.getItem(email);
+  // },
   completeInput(element) {
-    // const input = this.data.inputs[element.id];
-
-    // element.setCustomValidity('');
-    // element.checkValidity();
-
-    // input.completed = Boolean(input.value);
-
-    // if (input.value) {
-    //   input.error = !element.validity.valid;
-    // }
     element.setCustomValidity('');
     element.checkValidity();
-    console.log(element.validity);
+
+    return element.validity.valid && !element.validity.valueMissing;
   },
   inputChange(element) {
-    // const input = this.data.inputs[element.id];
-
-    // input.completed = false;
-
-    // if (!input.value) {
-    //   input.error = false;
-    // }
     element.setCustomValidity('');
     element.checkValidity();
     console.log(element.validity);
+
+    return element.validity.valid;
   },
   showError(element) {
     if (!element.value) {
       element.setCustomValidity(`Enter the ${element.id}`);
     }
   },
-  submit() {
-    if (this.data.inputs.registration) {
-      localStorage.setItem(this.data.inputs.email.value, this.data.inputs.password.value);
-      this.data.inputs.registration = false;
+  
+  // submit() {
+  //   if (this.data.inputs.registration) {
+  //     localStorage.setItem(this.data.inputs.email.value, this.data.inputs.password.value);
+  //     this.data.inputs.registration = false;
 
-      return;
-    }
+  //     return;
+  //   }
 
-    if (!this.verifyUser()) {
-      this.data.inputs.registration = true;
-      this.data.inputs.message = 'Registration';
-    } else {
-      this.data.inputs.email.error = false;
-    }
+  //   if (!this.verifyUser()) {
+  //     this.data.inputs.registration = true;
+  //     this.data.inputs.message = 'Registration';
+  //   } else {
+  //     this.data.inputs.email.error = false;
+  //   }
 
-    if (!this.verifyPassword()) {
-      this.data.inputs.password.error = true;
-      this.data.inputs.password.value = '';
-      this.data.inputs.password.completed = false;
-      this.data.inputs.message = 'Incorrect password';
-    } else {
-      this.data.inputs.email.error = false;
-      this.data.inputs.email.completed = false;
-      this.data.inputs.email.value = '';
-      this.data.inputs.password.error = false;
-      this.data.inputs.password.completed = false;
-      this.data.inputs.password.value = '';
+  //   if (!this.verifyPassword()) {
+  //     this.data.inputs.password.error = true;
+  //     this.data.inputs.password.value = '';
+  //     this.data.inputs.password.completed = false;
+  //     this.data.inputs.message = 'Incorrect password';
+  //   } else {
+  //     this.data.inputs.email.error = false;
+  //     this.data.inputs.email.completed = false;
+  //     this.data.inputs.email.value = '';
+  //     this.data.inputs.password.error = false;
+  //     this.data.inputs.password.completed = false;
+  //     this.data.inputs.password.value = '';
 
-      this.data.inputs.message = "You're are signed";
-    }
-  },
+  //     this.data.inputs.message = "You're are signed";
+  //   }
+  // },
 });
 
-Alpine.store('condition_methods', {
-  isAnyPropertyTrue(prop) {
-    return Object.values(Alpine.store('data').inputs).some((currentInput) => currentInput[prop]);
-  },
-});
+// Alpine.store('condition_methods', {
+//   isAnyPropertyTrue(prop) {
+//     return Object.values(Alpine.store('data').inputs).some((currentInput) => currentInput[prop]);
+//   },
+// });
 
 Alpine.start();
--- tailwindcss.config.js
+++ tailwindcss.config.js
@@ -1,19 +1,25 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   mode: 'jit',
   purge: ['./src/*.{html,js,css}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: colors.white,
+        gray: '#A9A9A9',
+        light: '#F1F1F4',
+      },
+    },
     fontSize: {
       xs: '.65rem',
     },
-    textColor: {
-      white: '#FFF',
-      black: '#000',
-      gray: '#A9A9A9',
-    },
     maxWidth: {
       xs: '16rem',
+    },
+    borderWidth: {
+      DEFAULT: '1px',
     },
   },
   variants: {
