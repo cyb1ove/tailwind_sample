177933f3b4e9f099016b253c401033ae232edfc4
cyb1ove <cyb1ove.ua@gmail.com>
2021-12-17 19:48:14 +0200

    .

--- src/index.js
+++ src/index.js
@@ -4,92 +4,95 @@ import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
-Alpine.store('behavior_methods', {
-  completeInput(element) {
-    element.setCustomValidity('');
-    element.checkValidity();
+function getMethods() {
+  return {
+    completeInput(element) {
+      element.setCustomValidity('');
+      element.checkValidity();
 
-    return element.validity.valid && !element.validity.valueMissing;
-  },
-  inputChange(element) {
-    element.setCustomValidity('');
-    element.checkValidity();
-    console.log(element.validity);
+      return element.validity.valid && !element.validity.valueMissing;
+    },
+    inputChange(element) {
+      element.setCustomValidity('');
+      element.checkValidity();
+      console.log(element.validity);
 
-    return element.validity.valid;
-  },
-  showError(element) {
-    if (!element.value) {
-      element.setCustomValidity(`Enter the ${element.id}`);
-    }
-  },
-  clearInput(input) {
-    input.value = '';
-    input.dispatchEvent(new Event('input'));
-  },
-  login(password, form) {
-    if (!password) {
+      return element.validity.valid;
+    },
+    showError(element) {
+      if (!element.value) {
+        element.setCustomValidity(`Enter the ${element.id}`);
+      }
+    },
+    clearInput(input) {
+      input.value = '';
+      input.dispatchEvent(new Event('input'));
+    },
+    login(password, form) {
+      if (!password) {
+        this.clearInput(form.password);
+
+        return form.email.id;
+      }
+
+      if (password !== form.password.value) {
+        console.log(password, form.password.value);
+        return form.password.id;
+      }
+
+      this.clearInput(form.email);
       this.clearInput(form.password);
+      console.log('Success');
 
-      return form.email.id;
-    }
+      return false;
+    },
+    registration(password, form) {
+      if (password) {
+        this.clearInput(form.password);
+        this.clearInput(form.repassword);
 
-    if (password !== form.password.value) {
-      console.log(password, form.password.value);
-      return form.password.id;
-    }
+        return form.email.id;
+      }
 
-    this.clearInput(form.email);
-    this.clearInput(form.password);
-    console.log('Success');
+      if (form.password.value !== form.repassword.value) {
+        return form.repassword.id;
+      }
 
-    return false;
-  },
-  registration(password, form) {
-    if (password) {
-      this.clearInput(form.password);
-      this.clearInput(form.repassword);
+      localStorage.setItem(form.email.value, form.password.value);
+      window.location.href = 'login.html';
 
-      return form.email.id;
-    }
+      return false;
+    },
+    emailForgottenPassword(password, form) {
+      if (!password) {
+        this.clearInput(form.email);
 
-    if (form.password.value !== form.repassword.value) {
-      return form.repassword.id;
-    }
+        return form.email.id;
+      }
 
-    localStorage.setItem(form.email.value, form.password.value);
-    window.location.href = 'login.html';
-
-    return false;
-  },
-  emailForgottenPassword(password, form) {
-    if (!password) {
       this.clearInput(form.email);
 
-      return form.email.id;
-    }
+      return false;
+    },
+    submit(form) {
+      const password = localStorage.getItem(form.email.value);
 
-    this.clearInput(form.email);
+      if (form.id === 'Log in') {
+        return this.login(password, form);
+      }
 
-    return false;
-  },
-  submit(form) {
-    const password = localStorage.getItem(form.email.value);
+      if (form.id === 'Sign up') {
+        return this.registration(password, form);
+      }
 
-    if (form.id === 'Log in') {
-      return this.login(password, form);
-    }
+      if (form.id === 'Email me') {
+        return this.emailForgottenPassword(password, form);
+      }
 
-    if (form.id === 'Sign up') {
-      return this.registration(password, form);
-    }
+      return new Error('Error');
+    },
+  };
+}
 
-    if (form.id === 'Email me') {
-      return this.emailForgottenPassword(password, form);
-    }
-
-    return new Error('Error');
-  },
-});
-
+Alpine.data('getMethods', getMethods);
 Alpine.start();
